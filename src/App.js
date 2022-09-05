f561d761d30d615fdaf27a8d0d92444a732065e4
Aly <princealy.nueles@gmail.com>
2022-09-05 10:49:23 +0800

    added feats

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbarr from './components/navbar';
+import Feat1 from './components/whyus'
+import Feat1p2 from './components/whyus2'
+import Feat2 from './components/howitworks'
+import Feat2p2 from './components/howitworks2'
 import HeroSection from './components/hero';
 import CardGrid from './components/cardgrid';
 // import BasicExample from  './components/cardsu';
@@ -21,6 +25,12 @@ function App() {
     
     <main>
       <CardGrid />
+      <br/>
+      <Feat1/>
+        <Feat1p2/>
+      <br/>
+        <Feat2/>
+        <Feat2p2/>
       <div>
       <TestiMonials></TestiMonials>
       </div>
