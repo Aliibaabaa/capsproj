62977043518dd92ec6984892b5a2b8bd5b38743e
Ben <ebzmangubat@gmail.com>
2022-09-05 09:26:40 +0800

    added multiple files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbarr from './components/navbar';
-import Feat1 from './components/whyus'
-import Feat1p2 from './components/whyus2'
-import Feat2 from './components/howitworks'
-import Feat2p2 from './components/howitworks2'
+import HeroSection from './components/hero';
+import CardGrid from './components/cardgrid';
+// import BasicExample from  './components/cardsu';
+import TestiMonials from './components/TestiMonials';
+
+
+
+
 
 function App() {
   return (
@@ -13,16 +17,18 @@ function App() {
     <nav>
       <Navbarr />
     </nav>
+    <header>
+      <HeroSection />
+    </header>
     
-    <main> 
-      <br/>
-        <Feat1/>
-        <Feat1p2/>
-      <br/>
-        <Feat2/>
-        <Feat2p2/>
+    <main>
+      <CardGrid />
+      <div>
+      <TestiMonials></TestiMonials>
+      </div>
+      
     </main>
-
+    
     <footer>
 
     </footer>
